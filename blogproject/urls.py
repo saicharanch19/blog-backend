ad4470eec9c2da5e2474f7ebac5506bafb4e76c6
Chedudeepu Sai Charan <saicharanch19@gmail.com>
2025-06-14 22:19:19 +0530

    Added root route for health check

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({'message': 'Blog backend is live!'})),  # 👈 Root endpoint
     path('admin/', admin.site.urls),
     path('api/', include('blogapp.urls')),  # your blog APIs
     path('auth/jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -15,5 +17,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),             
     path('auth/', include('djoser.urls.jwt')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
