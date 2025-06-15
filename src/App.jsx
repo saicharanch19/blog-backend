fd62dde185a6434a3e2f927c93c7e083f1d570f1
Chedudeepu Sai Charan <saicharanch19@gmail.com>
2025-06-15 17:07:59 +0530

    Ready for deployment

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+﻿export default function App() {
+  return <h1>My Blog Application</h1>
+}
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+﻿// @ts-check
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
